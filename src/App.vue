a893a286f62a258dc9634f36df8038533d9c147d
sterlingh78 <sterlingh78@yahoo.com>
2023-03-01 10:49:11 -0700

    added minimum app requirements

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Workout from './components/Workout.vue'
 
 <template>
     <!-- Default View listing workouts-->
-    <div v-if="state === null" class="hero min-h-screen card shadow-2xl bg-base-100">
+    <div v-if="state === null" class="hero card shadow-2xl bg-base-100">
       <div class="hero-content flex-col">
         <div class="text-center">
           <h1 class="text-5xl font-bold">Weightlifting App</h1>
@@ -13,16 +13,11 @@ import Workout from './components/Workout.vue'
         <div>
           <div class="card-body">
             <div  v-for="(workout, index) in this.workouts" :key="workout.id" class="m-2">
-              <div class="indicator">
-                <span class="indicator-item badge badge-secondary">{{ workout.date }}</span>
-                <div>
-                  <button class="btn btn-primary">{{ workout.title }}</button>
-                </div>
-              </div>
+              <Workout @click="handleWorkoutClick(index)" v-bind:workoutObj="workout" />
             </div>
           </div>
           <div class="form-control mt-6">
-            <button @click="addWorkout" class="btn btn-secondary">Add New</button>
+            <button @click="this.state = 'add'" class="btn btn-secondary">Add New</button>
           </div>
         </div>
       </div>
@@ -54,6 +49,79 @@ import Workout from './components/Workout.vue'
         </div>
       </div>
     </div>
+    <!-- Single Workout Detail View -->
+    <div v-if="state === 'workout'" class="rounded-md hero bg-base-200">
+      <div class="hero-content text-center">
+        <div>
+          <h1 class="text-5xl font-bold">{{ this.workouts[this.selected].title }}</h1>
+          <h2>{{ this.workouts[this.selected].date }}</h2>
+          <div class="overflow-x-auto">
+            <table class="table w-full">
+              <!-- head -->
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Exercise</th>
+                  <th>Sets</th>
+                  <th>Reps</th>
+                  <th>Weight</th>
+                </tr>
+              </thead>
+              <tbody v-for="(exercise, index) in this.workouts[this.selected].exercises" :key="exercise.id">
+                <tr>
+                  <th>{{ exercise.id }}</th>
+                  <td>{{ exercise.name }}</td>
+                  <td>{{ exercise.sets }}</td>
+                  <td>{{ exercise.reps }}</td>
+                  <td>{{ exercise.weight }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <button @click="this.state = 'addExercise'" class="btn btn-secondary mt-2 mr-1">Add Exercise</button>
+          <button @click="this.state = null" class="btn btn-secondary mt-2 ml-1">Back to Workouts</button>
+        </div>
+      </div>
+    </div>
+    <!-- Add Exercise View -->
+    <div v-if="state === 'addExercise'" class="hero">
+      <div class="hero-content flex-col">
+        <div class="text-center">
+          <h1 class="text-5xl font-bold">Add New Exercise</h1>
+        </div>
+        <div class="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+          <div class="card-body">
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Name</span>
+              </label>
+              <input v-model="newExercise.name" type="text" class="input input-bordered" />
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Sets</span>
+              </label>
+              <input v-model="newExercise.sets" type="text" class="input input-bordered" />
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Reps</span>
+              </label>
+              <input v-model="newExercise.reps" type="text" class="input input-bordered" />
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Weight</span>
+              </label>
+              <input v-model="newExercise.weight" type="text" class="input input-bordered" />
+            </div>
+            <div class="form-control mt-6">
+              <button @click="handleAddExercise" class="btn btn-secondary">Add</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -67,32 +135,13 @@ import Workout from './components/Workout.vue'
           title: '',
           date: ''
         },
-        workouts: [
-          {
-            id: 1,
-            title: "Workout 1",
-            date: 'Jan 1',
-            exercises: []
-          },
-          {
-            id: 2,
-            title: "Workout 2",
-            date: 'Jan 2',
-            exercises: []
-          },
-          {
-            id: 3,
-            title: "Workout 3",
-            date: 'Jan 3',
-            exercises: []
-          },
-          {
-            id: 4,
-            title: "Workout 4",
-            date: 'Jan 4',
-            exercises: []
-          }
-        ]
+        newExercise: {
+          name: '',
+          sets: '',
+          reps: '',
+          weight: ''
+        },
+        workouts: []
       }
     },
     methods: {
@@ -107,8 +156,22 @@ import Workout from './components/Workout.vue'
           this.state = null
         }
       },
-      addWorkout() {
-        this.state = 'add'
+      handleWorkoutClick(i) {
+        this.state = 'workout'
+        this.selected = i
+      },
+      handleAddExercise() {
+        this.workouts[this.selected].exercises = [
+          ...this.workouts[this.selected].exercises,
+          {
+            id: this.workouts[this.selected].exercises.length + 1,
+            name: this.newExercise.name,
+            sets: this.newExercise.sets,
+            reps: this.newExercise.reps,
+            weight: this.newExercise.weight
+          }
+        ]
+        this.state = 'workout'
       }
     }
   }
--- src/components/Workout.vue
+++ src/components/Workout.vue
@@ -1,12 +1,17 @@
 <template>
-    
+    <div class="indicator">
+        <span class="indicator-item badge badge-secondary">{{ workoutObj.date }}</span>
+        <div>
+            <button class="btn btn-primary">{{ workoutObj.title }}</button>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'workout',
         props: {
-            workout: Object
+            workoutObj: Object
         },
         data() {
             return {
